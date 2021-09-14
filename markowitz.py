378d422bcfd836e1559808907d99f7c281854b75
Eduardo Teixeira <69489166+Eduardo281@users.noreply.github.com>
2021-09-14 17:03:06 -0300

    Adição da função solve_fronteira()
    Primeira implementação de uma função especializada para construir a fronteira ótima, sem a necessidade de rodar a função "solve()" várias vezes em um loop.

--- markowitz.py
+++ markowitz.py
@@ -18,14 +18,17 @@ class Carteira(object):
             print(f"Fundo {self.fundos[i]} ({self.descricao[i]}) --> {round(100*self.porcentagem[i],2)}%; R$ {round(C*self.porcentagem[i],2)} --> R$ {self.minFundos[i]}")
 
     def __repr__(self):
-        return ('''Carteira(Retorno={}, Risco={}), Qtd. de fundos={}, porcentagem={}'''.format(
-            self.Retorno, self.Risco, len(self.fundos), self.porcentagem))
+        return ('''Carteira(Retorno={}, Risco={}, Qtd. de fundos={},
+fundos={}, 
+porcentagem={}, 
+descricao={}, 
+minFundos={}
+'''.format(self.Retorno, self.Risco, len(self.fundos), self.fundos, self.porcentagem, self.descricao, self.minFundos))
 
     def __str__(self):
         return ('Um objeto Carteira com {} fundos'.format(len(self.fundos)))
 
 class Markowitz(object):
-
     def __init__(self, C, minRetorno, K_min, K_max, P_min, P_max, P_categorias, limites, 
         n, minFundos, sigma, media, descricao, obj_type = 'Risco', l = 1, log = 0):
         self.C = C
@@ -84,15 +87,15 @@ class Markowitz(object):
     def solve(self, time=None, heur=None):
         if(self.K_min > self.K_max):
             print('Inconsistencia nos valores de K_min VS K_max!')
-            return Carteira(0, 0, [], 0, [], [])
+            return Carteira(0, 0, [], [], [], [])
 
         if(self.P_min > self.P_max):
             print('Inconsistencia nos valores de P_min VS P_max!')
-            return Carteira(0, 0, [], 0, [], [])
+            return Carteira(0, 0, [], [], [], [])
 
         if(  round(sum(self.P_categorias), 5) < 1  ):
             print('Problema nos percentuais minimos para cada tipo de fundo!')
-            return Carteira(0, 0, [], 0, [], [])
+            return Carteira(0, 0, [], [], [], [])
 
         if(time != None):
             self.model.setParam('TimeLimit', time)
@@ -105,7 +108,7 @@ class Markowitz(object):
             self.w[0].X >= 0
         except:
             print('Nenhuma solucao encontrada!')
-            return Carteira(0, 0, [], 0, [], [])
+            return Carteira(0, 0, [], [], [], [])
         
         self.Retorno = np.dot(self.media, np.array([self.w[i].x for i in range(len(self.w))]))
         if (self.obj_type in self.modelo_classico_labels):
@@ -120,6 +123,40 @@ class Markowitz(object):
         self.apm_escolhidos = [self.minFundos[i] for i in self.fundos]
 
         return Carteira(self.Retorno, self.Risco, self.fundos, self.porcentagem, self.cnpj_escolhidos, self.apm_escolhidos)
+
+    def solve_fronteira(self, minRetornoInit, minRetornoFinal, tam_passo, taxa_livre_de_risco = 0.0525, 
+    time = 15, heur = None, log_sol = 0):
+        self.update_minRetorno(minRetornoInit)
+        
+        self.lista_retornos = list()
+        self.lista_riscos = list()
+        self.lista_sharpes = list()
+        self.lista_carteiras = list()
+        self.tam_passo = tam_passo
+
+        if(self.tam_passo <= 0):
+            self.tam_passo = 0.05
+            print('Tamanho do passo automaticamente ajustado para:', self.tam_passo)
+
+        self.carteira = Carteira(0, 0, [], [], [], [])
+
+        while self.minRetorno <= minRetornoFinal:
+            if(log_sol != 0):
+                print('minRetorno atual:', self.minRetorno)
+            self.carteira = self.solve(time, heur)
+            if(self.carteira.Retorno != 0):
+                self.lista_retornos.append(self.carteira.Retorno)
+                self.lista_riscos.append(self.carteira.Risco)
+                self.lista_sharpes.append((self.carteira.Retorno - taxa_livre_de_risco)/self.carteira.Risco)
+            else:
+                break
+            self.lista_carteiras.append(self.carteira)
+            self.minRetorno += self.tam_passo
+            self.update_minRetorno(self.minRetorno)
+
+        self.max_sharpe = np.argmax(self.lista_sharpes)
+
+        return self.lista_carteiras, self.max_sharpe    
 
     @property
     def P_min(self):
